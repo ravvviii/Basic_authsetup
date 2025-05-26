f1de399e0624a9643914f76bfecca2ece8c39809
Ravvviii <rs9625645@gmail.com>
2025-05-26 13:33:18 +0530

    user schema build

---
+++ models/user.js
@@ -0,0 +1,30 @@
+// User {
+//   username,
+//   email,
+//   passwordHash,
+//   role (user/admin),
+//   createdAt
+// }
+
+
+
+const mongoose = require('mongoose')
+
+
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+
+},{timestamps:true})
+
+module.exports = mongoose.model("User", userSchema);
