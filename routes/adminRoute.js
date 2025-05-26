245d80e6679dad0db9a8a22b46ecf63ae660e40f
Ravvviii <rs9625645@gmail.com>
2025-05-26 18:14:57 +0530

    full admin route setup and authentication and authorisation compalted

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -56,4 +56,71 @@ router.get("/getUser/:id", isLoggedIn,async(req, res)=>{
 })
 
 
+router.put("/updateUser/:id", isLoggedIn,async(req, res)=>{
+    try {
+         const role = req.user.role
+        if(role != "admin"){
+            return res.status(401).send("Unauthorised")
+        }
+        const id = req.params.id;
+        const response = await User.findById(id)
+        if(!response) {
+            return  res.status(500).json({ message: "No user found" })
+        }
+
+
+        const data = req.body;
+        const UpdatedDoc = await User.findByIdAndUpdate(id, data, {new:true})
+        if(!UpdatedDoc){
+            return  res.status(404).json({ message: "Post not found" })
+        }
+         console.log(`Post Updated`);
+        res.status(200).json(UpdatedDoc)
+
+
+
+
+
+
+    } catch (error) {
+        console.log(`Error while getting specific user: `, error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+
+
+
+
+
+router.delete("/deleteUser/:id", isLoggedIn,async(req, res)=>{
+    try {
+
+
+         const role = req.user.role
+        if(role != "admin"){
+            return res.status(401).send("Unauthorised")
+        }
+
+
+        const id = req.params.id;
+        const response = await User.findByIdAndDelete(id);
+        // console.log(req.user.role);
+        
+
+        if(!response){
+            return  res.status(404).json({ message: "User not found" })
+        }
+        console.log(`Post Deleted`);
+        res.status(200).json({message:"User deleted successfully"})
+        
+        
+    } catch (error) {
+          console.log(`Error while deleting post`);
+        res.status(500).json("Error")
+    }
+})
+
+
+
 module.exports = router
